a942d3eb79635993abde89066d66b2b0225dd003
Akhilesh Yadav <akhileshyadavkokta@gmail.com>
2021-01-25 19:25:01 +0530

    Commited

--- App1/views.py
+++ App1/views.py
@@ -2,14 +2,29 @@ from django.shortcuts import render,redirect
 from .models import *
 from .form import ContactForm
 from django.contrib import messages
+from django.core.paginator import Paginator
 
 # Create your views here.
 def Home(request):
     cate=Category.objects.all()
-    images=Image.objects.all()
-    context={'images':images,'cate':cate,
+    images=Image.objects.all().order_by('-added_date')
+    paginator=Paginator(images,6)
+    page_number=request.GET.get('page')
+    page_obj=paginator.get_page('page_number')
+    context={'images':images,'cate':cate,'page_obj':page_obj
     }
     return render(request,'App1/Home.html',context)
+
+# def blog(request):
+#     posts = Post.objects.all().order_by('-posted_date')
+#     paginator = Paginator(posts, 2)
+
+#     page_number = request.GET.get('page')
+#     page_obj = paginator.get_page(page_number)
+
+#     context = {'page_obj': page_obj}
+#     return render(request, 'main/blog.html', context)
+
 
 def category(request,cid):
     cate=Category.objects.all()
