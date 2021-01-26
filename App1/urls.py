d3b079d757580dd2d7add3f442f686de716076ca
Akhilesh Yadav <akhileshyadavkokta@gmail.com>
2021-01-26 10:58:01 +0530

    commited

--- App1/urls.py
+++ App1/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from .import views
+
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('',views.Home,name='Home'),
     path('category/<int:cid>/',views.category,name='category'),
     path("contact/",views.contact,name='contact'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_ROOT, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_ROOT, document_root=settings.MEDIA_ROOT)
